3045b5a420404dc26f0ffd9c949521921279004c
Joseph Sang <jsang275@gmail.com>
2026-07-10 13:44:45 +0300

    Updated build/run configs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,8 +2,8 @@ import { DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { SQLiteProvider } from 'expo-sqlite';
 import { Provider } from 'react-redux';
+import { useEffect } from 'react';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
 import { migrateDbIfNeeded } from '@/database/schema';
 import { useConnectivity } from '@/hooks/useConnectivity';
@@ -16,13 +16,20 @@ function ConnectivityListener() {
   return null;
 }
 
+function SplashHider() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+  return null;
+}
+
 export default function TabLayout() {
   return (
     <SQLiteProvider databaseName="triage.db" onInit={migrateDbIfNeeded}>
       <Provider store={store}>
         <ThemeProvider value={DefaultTheme}>
+          <SplashHider />
           <ConnectivityListener />
-          <AnimatedSplashOverlay />
           <AppTabs />
         </ThemeProvider>
       </Provider>
